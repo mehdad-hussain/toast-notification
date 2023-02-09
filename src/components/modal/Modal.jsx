ec3cc72366a2cd88365b9e5cb57971fa22ddec63
mehdad-hussain <hussain.mehdad1@gmail.com>
2023-02-09 19:55:00 +0600

    9/2 - modal content css problem fixed

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -8,7 +8,7 @@ export const Modal = (props) => {
       {/*content*/}
       <div className="w-auto max-w-sm mx-auto bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none modal">
         {/*header*/}
-        <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+        <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200 modal-content">
           <h3 className="text-3xl font-semibold">Modal Title</h3>
           <button
             className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
@@ -20,7 +20,7 @@ export const Modal = (props) => {
           </button>
         </div>
         {/*body*/}
-        <div className="relative flex-auto p-6">
+        <div className="relative flex-auto p-6 modal-content">
           <p className="my-4 text-lg leading-relaxed text-slate-500">
             I always felt like I could do anything. That’s the main thing people
             are controlled by! Thoughts- their perception of themselves!
@@ -44,7 +44,7 @@ export const Modal = (props) => {
           ></rect>
         </svg>
         {/*footer*/}
-        <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+        <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200 modal-content">
           <button
             className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none cursor-pointer background-transparent focus:outline-none"
             type="button"
